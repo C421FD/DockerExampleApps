e942495f3656ffe0e3b60fad3f0e913294104d1a
Ванюшкин Максим (Vanyushkin_MV) <Vanyushkin_MV@directum.ru>
2018-03-27 17:20:51 +0400

    Initial commit

---
+++ ServiceA/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceA.Controllers
+{
+  [Route("api/[controller]")]
+  public class ValuesController : Controller
+  {
+    // GET api/values
+    [HttpGet]
+    public async Task<string> Get()
+    {
+      using (var httpClient = new HttpClient())
+      {
+        var result = await httpClient.GetAsync("http://localhost:10001/api/values");
+        return await result.Content.ReadAsStringAsync();
+      }
+    }
+  }
+}
---
+++ ServiceC/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceC.Controllers
+{
+  [Route("api/[controller]")]
+  public class ValuesController : Controller
+  {
+    // GET api/values
+    [HttpGet]
+    public string Get()
+    {
+      return "Hello from Service C!";
+    }
+  }
+}
